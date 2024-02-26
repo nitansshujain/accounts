4b3772713c9e3e5bbbef1c96cf26a325d8db3c97
nitanssj <nitanssj@amdocs.com>
2024-02-26 19:52:23 +0530

    Refresh configuration at runtime using refresh actuator path

--- src/main/java/com/microservice/accounts/dto/AccountsContactInfoDto.java
+++ src/main/java/com/microservice/accounts/dto/AccountsContactInfoDto.java
@@ -1,11 +1,22 @@
 package com.microservice.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//@ConfigurationProperties(prefix = "accounts")
+// Using record will not allow setters, hence refreshing the values at runtime will not be possible
+//public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//}
 
+@ConfigurationProperties(prefix = "accounts")
+@Getter
+@Setter
+public class AccountsContactInfoDto{
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
